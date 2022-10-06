029b7042aa439300afc35974f5b02d9893622735
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-10-06 15:50:22 +0800

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,4 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Log = loadstring(game:HttpGet("https://raw.githubusercontent.com/Belkworks/synlog/master/init.lua", "Log"))()
 local Window = Library.CreateLib("Noob's GUI Script Hub", "Synapse")
 local me = Window:NewTab("Home")
 local Home = me:NewSection("Welcome to Noob's Script Hub GUI, "..game.Players.LocalPlayer.Name)
@@ -42,4 +43,5 @@ end)
 sets:NewTextBox("Key", "Key for scripts", function(txt)
 		print(txt)
 		key = txt
+		Log:print("Key srt!")
 end)
