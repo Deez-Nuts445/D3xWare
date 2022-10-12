3d657316db029631d1d8a9a7d9239833bbb4e3ce
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-10-12 20:41:03 +0800

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,1 +1,1 @@
-local q = loadstring(game:HttpGet("https://gist.githubusercontent.com/Deez-Nuts445/42a0ca5750c008c6ef0db3e65e10db03/raw/2007cabbf391e7594c7ff32ebed4d1ba4698b2d4/'kay.lua")
+loadstring(game:HttpGet("https://gist.githubusercontent.com/Deez-Nuts445/42a0ca5750c008c6ef0db3e65e10db03/raw/2007cabbf391e7594c7ff32ebed4d1ba4698b2d4/'kay.lua")
