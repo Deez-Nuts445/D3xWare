c9914be08a2816f040ada3fc9faa90bb2a3c1b2c
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-10-22 17:16:27 +0800

    Update src.lua

--- src.lua
+++ src.lua
@@ -1,4 +1,74 @@
--- Script Obfuscated by Noob's SOB(Script Obfuscator).
--- .gg/6kp4eSMq8k
--- 24490701
-local o = loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\120\72\101\112\116\99\47\75\97\118\111\45\85\73\45\76\105\98\114\97\114\121\47\109\97\105\110\47\115\111\117\114\99\101\46\108\117\97"))() local i = loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\66\101\108\107\119\111\114\107\115\47\115\121\110\108\111\103\47\109\97\115\116\101\114\47\105\110\105\116\46\108\117\97", "\76\111\103"))() local s = o.CreateLib("\78\111\111\98\39\115\32\83\99\114\105\112\116\32\72\117\98\32\71\85\73", "\77\105\100\110\105\103\104\116") local d = s:NewTab("\72\111\109\101") local l = d:NewSection("\87\101\108\99\111\109\101\32\116\111\32\78\111\111\98\39\115\32\83\99\114\105\112\116\32\72\117\98\32\71\85\73\44\32"..game.Players.LocalPlayer.Name.."\33") local e = s:NewTab("\77\121\32\83\99\114\105\112\116\115") local r = s:NewTab("\79\116\104\101\114\32\83\99\114\105\112\116\115") local h = s:NewTab("\83\101\116\116\105\110\103\115") local j = h:NewSection("\85\73\32\83\101\116\116\105\110\103\115") local c = e:NewSection("\79\50\32\83\99\114\105\112\116\32\72\117\98") local n  c:NewDropdown("\83\99\114\105\112\116\115\32\40\68\114\111\112\100\111\119\110\41", "\65\32\76\105\98\114\97\114\121\32\111\102\32\115\99\114\105\112\116\115\32\111\110\32\79\50\32\70\69\32\83\99\114\105\112\116\32\72\117\98", {"\105\121","\103\101\116\106\111\98\105\100", "\97\110\116\105\103\114\97\118", "\101\120\105\116", "\117\116\103","\110\111\116\101\115\103\117\105", "\119\97\108\108\119\97\108\107", "\117\116\103", "\103\97\109\101\103\117\105", "\98\97\108\108", "\99\104\111\111\115\101\109\97\112", "\97\108\108\116\111\111\108\115", "\115\104\111\119\103\108\97\115\115", "\112\108\97\100\109\105\110", "\97\100\109\105\110", "\102\101\99\104\101\99\107", "\114\101\97\99\104", "\102\108\105\110\103\103\117\105","\119\104\97\116\101\120\112\108\111\116\115\97\114\101\111\110\108\105\110\101"}, function(a) function k(g,t) getgenv().f = g getgenv().m = t loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\68\101\101\122\45\78\117\116\115\52\52\53\47\79\50\45\70\69\45\83\99\114\105\112\116\45\104\117\98\47\109\97\105\110\47\115\114\99"))() end k(a,true) end) local p = e:NewSection("\67\67\83\32\50\32\67\97\114\32\83\112\97\119\110\101\114") p:NewButton("\82\117\110","\82\117\110\32\83\112\97\119\110\101\114",function() loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\68\101\101\122\45\78\117\116\115\52\52\53\47\78\111\111\98\45\115\45\83\99\114\105\112\116\45\72\117\98\45\71\85\73\47\109\97\105\110\47\116\46\108\117\97"))() end) local b = e:NewSection("\82\101\109\111\116\101\32\83\112\121") p:NewButton("\82\117\110","\82\117\110\32\82\101\109\111\116\101\83\112\121",function() loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\68\101\101\122\45\78\117\116\115\52\52\53\47\78\111\111\98\45\115\45\83\99\114\105\112\116\45\72\117\98\45\71\85\73\47\109\97\105\110\47\114\115\112\121\46\108\117\97"))() end) local f = r:NewSection("\90\77\111\100\32\118\50\46\51") f:NewButton("\82\117\110", "\90\77\111\100\32\118\50\46\51", function() loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\68\101\101\122\45\78\117\116\115\52\52\53\47\71\72\83\97\110\100\98\111\120\95\76\85\65\95\49\47\109\97\105\110\47\86\77\111\100"))() end) local _ = r:NewSection("\68\97\114\107\32\72\117\98") _:NewButton("\82\117\110", "\68\97\114\107\32\72\117\98", function() loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\68\101\101\122\45\78\117\116\115\52\52\53\47\71\72\83\97\110\100\98\111\120\95\76\85\65\95\49\47\109\97\105\110\47\100",true))() end) local k_ = r:NewSection("\83\101\114\118\101\114\32\72\111\112") k_:NewButton("\82\117\110", "\83\101\114\118\101\114\32\72\111\112", function() local gk = loadstring(game:HttpGet"\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\76\101\111\75\104\111\108\89\116\47\114\111\98\108\111\120\47\109\97\105\110\47\108\107\95\115\101\114\118\101\114\104\111\112\46\108\117\97")() gk:Teleport(game.PlaceId) loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/Noob's%20Script%20Hub%20Gui"))() end) local tg = r:NewSection("\79\112\101\110\32\65\117\116\111\99\108\105\99\107\101\114\32\71\85\73") tg:NewButton("\82\117\110", "\71\101\116\32\71\85\73", function() loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\68\101\101\122\45\78\117\116\115\52\52\53\47\71\72\83\97\110\100\98\111\120\95\76\85\65\95\49\47\109\97\105\110\47\65\117\116\111\67\108\105\99\107\101\114\71\117\105\46\108\117\97"))() end) local at = r:NewSection("\82\101\106\111\105\110\32\83\101\114\118\101\114") at:NewButton("\82\117\110", "\82\101\106\111\105\110\32\116\104\101\32\115\101\114\118\101\114", function() game:GetService("\84\101\108\101\112\111\114\116\83\101\114\118\105\99\101"):Teleport(game.PlaceId, game:GetService("\80\108\97\121\101\114\115").LocalPlayer) end) j:NewKeybind("\84\111\103\103\108\101\32\85\73", "\84\111\103\103\108\101\115\32\85\73", Enum.KeyCode.Home, function() o:ToggleUI() end) j:NewTextBox("\75\101\121", "\75\101\121\32\102\111\114\32\115\99\114\105\112\116\115", function(m) print(m) n = m i:print("\75\101\121\32\115\101\116\33") end) local ma = s:NewTab("\67\114\101\100\105\116\115") local om = ma:NewSection("\68\105\115\99\111\114\100") om:NewButton("\68\105\115\99\111\114\100","\74\111\105\110\32\116\104\101\32\100\105\115\99\111\114\100", function() getgenv().InviteCode = "\54\107\112\52\101\83\77\113\56\107" loadstring(game:HttpGet("\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\76\117\97\81\76\101\97\107\47\83\99\114\105\112\116\115\47\109\97\105\110\47\68\105\115\99\111\114\100\45\65\117\116\111\45\74\111\105\110\46\108\117\97"))()   end)
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Log = loadstring(game:HttpGet("https://raw.githubusercontent.com/Belkworks/synlog/master/init.lua", "Log"))()
+local Window = Library.CreateLib("Noob's Script Hub GUI", "Midnight")
+local me = Window:NewTab("Home")
+local Home = me:NewSection("Welcome to Noob's Script Hub GUI, "..game.Players.LocalPlayer.Name.."!")
+local Tab = Window:NewTab("My Scripts")
+local Tab2 = Window:NewTab("Other Scripts")
+local Tab3 = Window:NewTab("Settings")
+local sets = Tab3:NewSection("UI Settings")
+local Section = Tab:NewSection("O2 Script Hub")
+local key 
+
+Section:NewDropdown("Scripts (Dropdown)", "A Library of scripts on O2 FE Script Hub", {"iy","getjobid", "antigrav", "exit", "utg","notesgui", "wallwalk", "utg", "gamegui", "ball", "choosemap", "alltools", "showglass", "pladmin", "admin", "fecheck", "reach", "flinggui","whatexplotsareonline"}, function(currentOption)
+function loadScript(v,send)
+getgenv().f = v
+getgenv().m = send
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/O2-FE-Script-hub/main/src"))()
+end
+loadScript(currentOption,true)
+end)
+local Section2 = Tab:NewSection("CCS 2 Car Spawner")
+Section2:NewButton("Run","Run Spawner",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/Noob-s-Script-Hub-GUI/main/t.lua"))()
+end)
+local Section3 = Tab:NewSection("Remote Spy")
+Section3:NewButton("Run","Run RemoteSpy",function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/Noob-s-Script-Hub-GUI/main/rspy.lua"))()
+end)
+local dd = Tab2:NewSection("ZMod v2.3")
+dd:NewButton("Run", "ZMod v2.3", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/VMod"))()
+end)
+
+local js = Tab2:NewSection("Dark Hub")
+js:NewButton("Run", "Dark Hub", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/d",true))()
+end)
+
+local ky = Tab2:NewSection("Server Hop")
+ky:NewButton("Run", "Server Hop", function()
+local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
+
+module:Teleport(game.PlaceId)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/Noob's%20Script%20Hub%20Gui"))()
+end)
+
+local bb = Tab2:NewSection("Open Autoclicker GUI")
+bb:NewButton("Run", "Get GUI", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/AutoClickerGui.lua"))()
+end)
+
+
+local bh = Tab2:NewSection("Rejoin Server")
+bh:NewButton("Run", "Rejoin the server", function()
+game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer)
+end)
+
+
+sets:NewKeybind("Toggle UI", "Toggles UI", Enum.KeyCode.Home, function()
+	Library:ToggleUI()
+end)
+sets:NewTextBox("Key", "Key for scripts", function(txt)
+		print(txt)
+		key = txt
+		Log:print("Key set!")
+end)
+
+
+local Credits = Window:NewTab("Credits")
+local Section0 = Credits:NewSection("Discord")
+Section0:NewButton("Discord","Join the discord", function()
+getgenv().InviteCode = "6kp4eSMq8k"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaQLeak/Scripts/main/Discord-Auto-Join.lua"))()		
+end)
