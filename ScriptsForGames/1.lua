1b35ead62a2cde5b57033e11392b7abeae7b75ad
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-12-19 19:47:50 +0800

    Update 1.lua

--- ScriptsForGames/1.lua
+++ ScriptsForGames/1.lua
@@ -1,14 +1,13 @@
-local DiscordLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/discord-ui.lua")()
+local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/Deez-Nuts445/GHSandbox_LUA_1/main/discord-ui.lua")()
 local w = DiscordLib:Window("D3xWare Games Hub")
-local s = w:Server("Home")
+local s = w:Server("Home",0)
 local c1 = s:Channel("Scripts")
 c1:Button("Reveal Doors",function()
 for k, v in pairs(game:GetService("Workspace").Doors:GetDescendants()) do -- For Loop
     if v.Name == "???" then  -- Check for isntance
         local cor_part = v.Parent
-        cor_part.Color3 = Color3.new(0,255,0)
-        cor_part.Material = "Neon"            
+         cor_part.BrickColor = BrickColor.new("Dark green") -- Change the color of the parent
+        cor_part.Material = "Neon" -- Explained
     end
 end    
 end)
-
