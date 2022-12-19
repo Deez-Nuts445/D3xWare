74a7b5c858a34175c3c47fe9650723fb696bd1ee
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-12-19 19:33:16 +0800

    Update game-hub.lua

--- game-hub.lua
+++ game-hub.lua
@@ -1,11 +1,9 @@
 local list = {
-
-["10679961467"] = "https://raw.githubusercontent.com/Deez-Nuts445/D3xWare/main/ScriptsForGames/1.lua"
-
+    ["10679961467"] = "https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/50-50-Pick-A-Door-Obby/main.lua";
 }
+
 for id, url in next, list do
     if string.find(id, game.PlaceId) then
-        loadstring(game:HttpGet(url))(); 
-        break
+        loadstring(game:HttpGet(url))(); break
     end
 end
