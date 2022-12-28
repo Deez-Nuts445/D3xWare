f995d5bb8424b7e0917b338d4c73536699291000
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2022-12-28 22:41:51 +0800

    Update game-hub.lua

--- game-hub.lua
+++ game-hub.lua
@@ -1,5 +1,6 @@
 local list = {
     ["10679961467"] = "https://raw.githubusercontent.com/Deez-Nuts445/D3xWare/main/ScriptsForGames/1.lua";
+    ["189707"] = 'https://raw.githubusercontent.com/Deez-Nuts445/D3xWare/main/ScriptsForGames/2.lua'
 }
 
 for id, url in next, list do
