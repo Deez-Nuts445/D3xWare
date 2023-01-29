d8c86f605c7e34dd7285fbe27ad2ce71b799e83a
Deez-Nuts445 <111050099+Deez-Nuts445@users.noreply.github.com>
2023-01-29 13:49:02 +0800

    Update game-hub.lua

--- game-hub.lua
+++ game-hub.lua
@@ -1,10 +1,1 @@
-local list = {
-    ["10679961467"] = "https://raw.githubusercontent.com/Deez-Nuts445/D3xWare/main/ScriptsForGames/1.lua";
-    ["189707"] = 'https://raw.githubusercontent.com/Deez-Nuts445/D3xWare/main/ScriptsForGames/2.lua'
-}
-
-for id, url in next, list do
-    if string.find(id, game.PlaceId) then
-        loadstring(game:HttpGet(url))(); break
-    end
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D3x-Scripts/D3xWare/main/game-hub.lua"))()
